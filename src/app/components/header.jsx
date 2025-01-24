ee85e4d188be48698b34a52afde8cd4816540a66
kvngjaypee <johnpaulanyanwu17@gmail.com>
2025-01-24 18:49:55 +0100

    touched page.js

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 export const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>I am Coding</h1>
+      </div>
+    </div>
+  );
 };
 export default Header;

332e0804b270b56a03cd43edf982182ea339a725
Arthakker <arthuronyeanusi@gmail.com>
2025-03-14 03:17:54 +0100

    Initialized the auth processes

--- src/app/pages/api/auth/[...nextauth].js
+++ src/app/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { sanityClient } from "@/lib/sanity";
 import bcrypt from "bcryptjs";
 
 export default NextAuth({
@@ -12,16 +13,27 @@ export default NextAuth({
       },
       async authorize(credentials) {
         const query = `*[_type == "user" && email == $email][0]`;
-        const params = { email: credentials.email };
+        const user = await sanityClient.fetch(query, {
+          email: credentials.email,
+        });
 
-        const response = await fetch(
-          `https://y5zakwvd.api.sanity.io/v2024-03-09/data/query/production?query=${encodeURIComponent(
-            query
-          )}`,
-          { headers: { Authorization: `Bearer ${process.env.SANITY_TOKEN}` } }
-        );
-        const { result: user } = await response.json();
+        if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+          throw new Error("Invalid credentials");
+        }
+
+        return { id: user._id, name: user.name, email: user.email };
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });

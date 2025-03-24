47252a9a00cd61995c35b81bd8daf9bf0c9c36f9
Edarth002 <arthuronyeanusi@gmail.com>
2025-03-24 18:45:22 +0100

    Corrected rendering issues with dashboard

--- src/app/pages/dashboard/page.jsx
+++ src/app/pages/dashboard/page.jsx
@@ -8,21 +8,28 @@ export const Dashboard = () => {
   const router = useRouter();
   const { user, logout } = useAuth();
 
-  //   useEffect(() => {
-  //     if (!user) {
-  //       router.push("/auth/login");
-  //     }
-  //   }, [user, router]);
+    useEffect(() => {
+      if (!user) {
+        router.push("/auth/login");
+      }
+    }, [user, router]);
+
+  if (!user) return null;
+
+  async function handleLogout(){
+    await logout();
+    router.replace("/auth/login")
+  }
 
   return (
     <div className="p-10">
-      Current User:
+      <h2>Current User:</h2>
       <div className="flex items-center space-x-5">
         <p>{user.username}</p>
         <p>{user.email}</p>
         <button
           className="bg-red-400 rounded-md text-white hover:bg-red-600 p-2"
-          onClick={logout}
+          onClick={handleLogout}
         >
           Logout
         </button>
